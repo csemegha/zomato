bdd6f8e283a9085855df3566824b9e2989148e38
csemegha <meghanarudresh039@gmail.com>
2025-10-23 16:58:03 +0530

    Add files via upload

---
+++ evenodd.js
@@ -0,0 +1,10 @@
+function checkEvenOdd(num) {
+    if (num % 2 === 0) {
+        console.log(num, "isEven");
+    }
+    else {
+        console.log(num, "isOdd");
+    }
+}
+checkEvenOdd(8);
+checkEvenOdd(13);
